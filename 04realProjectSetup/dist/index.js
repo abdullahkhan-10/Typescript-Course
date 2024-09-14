d1a112bed2a44b068e978d14dcd20c988309b2b3
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-09-14 23:48:54 +0500

    Real project setup tutorial added

---
+++ 04realProjectSetup/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Typescript is set to project");
+console.log("More typescript is added in watch mode");
---
+++ 04realProjectSetup/src/index.ts
@@ -0,0 +1,2 @@
+console.log("Typescript is set to project");
+console.log("More typescript is added in watch mode");
