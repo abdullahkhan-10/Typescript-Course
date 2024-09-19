63aa0c97c9fa86295916e69e858efd48649bad3c
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-09-20 00:26:16 +0500

    Narrowing tutorial the in operator topic added

--- 03advance/20narrowing.ts
+++ 03advance/20narrowing.ts
@@ -1,4 +1,5 @@
 // ----------------------------------------- Narrowing -----------------------------------------
+// To specify the type
 
 function detectType(val: number | string){
     if (typeof val === "string") {
@@ -13,12 +14,44 @@ detectType("Abdullah")
 // detectType(10)
 
 // ------------ null keyword ----------------
-// The (null) keyword is used for caution, and we will do quite alot of time in regular code.
-// the null works in case if some do not pass any string value.
+// The (null) keyword is used for caution, and we will use quite alot of time in regular code.
+// the null keyword works in case if some one do not pass any string value.
+
 function provideId(id: string | null){
+    // if someone do not pass an id execute code insie if statement.
     if(!id){
         console.log("Please provide id");
         return
     }
+
+    // if some one pass id the do this.
     id.toUpperCase()
+}
+
+
+// ------------ The in operaor ----------------
+// It determining if an (interface) has a property with a name: the in operator. TypeScript takes this into account as a way to narrow down potential types.
+
+interface User{
+    name: string,
+    email: string
+    isLoggedIn: boolean
+}
+
+interface Admin{
+    name: string,
+    email: string,
+    isAdmin: boolean
+}
+
+function isAdminAccount(account: User | Admin){
+    // The in operator check if the property is available in any interface, we will have the access of that interface in parameter (account). This how we can narrow down the type.
+    if ("isAdmin" in account) {
+        return account.isAdmin
+    }
+
+    // In this case we have access to interface (User).
+    if ("isLoggedIn" in account) {
+        return account.isLoggedIn
+    }
 }
