051f59a91132e338696c07768a9588546498ca21
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-09-19 23:57:49 +0500

    Narrowing tutorial null keyword topic added

--- 03advance/20narrowing.ts
+++ 03advance/20narrowing.ts
@@ -2,7 +2,7 @@
 
 function detectType(val: number | string){
     if (typeof val === "string") {
-        return val.toLocaleLowerCase
+        return val.toLowerCase()
     }
     return val + 10
 }
@@ -12,4 +12,13 @@ detectType("Abdullah")
 //  if we pass (number) as argument then (val) will act as  type number, and will execute code outside if statement.
 // detectType(10)
 
-// The (null) keyword
+// ------------ null keyword ----------------
+// The (null) keyword is used for caution, and we will do quite alot of time in regular code.
+// the null works in case if some do not pass any string value.
+function provideId(id: string | null){
+    if(!id){
+        console.log("Please provide id");
+        return
+    }
+    id.toUpperCase()
+}
