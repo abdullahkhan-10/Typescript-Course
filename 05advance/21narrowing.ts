b7e079586266ad39760b05362313126ee5e27697
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-09-21 00:06:30 +0500

    Narrowing tutorial instanceOf and type predicate topics added

--- 05advance/21narrowing.ts
+++ 05advance/21narrowing.ts
@@ -12,3 +12,31 @@ function logValue(x: Date | string) {
       console.log(x.toUpperCase());
     }
 }
+
+// -------------------------------- Type Predicate -----------------------------------
+// Sometimes we want more direct control over how types change throughout our code.
+// To define a user-defined type, we simply need to define a function whose return type is a type predicate:
+
+type Fish = {
+    swim: ()=> void
+}
+
+type Bird = {
+    fly: () => void
+}
+
+// the (pet is Fish) syntax is type predicate.
+function isFish(pet: Fish | Bird): pet is Fish{
+    return (pet as Fish).swim !== undefined
+}
+
+// We still don't know whether the pet is type Fish or type Bird, To exactly know we need to explicitly return (pet is Fish) from the function (isFish)
+function getFood(pet: Fish | Bird){
+    if (isFish(pet)) {            
+        pet                   // now the pet will have type Fish
+        return "fish food"
+    }else{
+        pet 
+        return "bird food"
+    }
+}
